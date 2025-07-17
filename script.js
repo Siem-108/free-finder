a08847c2112e4b561b76904deab33e270c0f79e9
Siem-108 <siemestifanos108@gmail.com>
2025-07-17 20:18:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-function sendMail(){
-    let parms = {
-        name : document.getElementById("name"),value,
-        email : document.getElementById("email").value,
-        subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value,
-
-    }
-    emailjs.send("service_t5qcgjv","template_f1tvom3").then(alert("Email Sent!!!"))
-}
+<form onsubmit="sendMail(event)">
+  <div class="form-group">
+    <label for="name">Name</label>
+    <input type="text" id="name" name="name" required />
+  </div>
+  <div class="form-group">
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email" required />
+  </div>
+  <div class="form-group">
+    <label for="Subject">Subject</label>
+    <input type="text" id="Subject" name="Subject" required />
+  </div>
+  <div class="form-group">
+    <label for="message">Message</label>
+    <textarea id="message" name="message" required></textarea>
+  </div>
+  <button type="submit">Send</button>
+</form>
