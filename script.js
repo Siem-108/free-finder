994809128a445e4e5978cf806f3ce73faf318ee6
Siem-108 <siemestifanos108@gmail.com>
2025-07-17 21:01:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-
 <form onsubmit="sendMail(event)">
-    
   <div class="form-group">
     <label for="name">Name</label>
     <input type="text" id="name" name="name" required />
   </div>
+
   <div class="form-group">
     <label for="email">Email</label>
     <input type="email" id="email" name="email" required />
   </div>
+
   <div class="form-group">
-    <label for="Subject">Subject</label>
-    <input type="text" id="Subject" name="Subject" required />
+    <label for="subject">Subject</label> 
+    <input type="text" id="subject" name="subject" required />
   </div>
+
   <div class="form-group">
     <label for="message">Message</label>
     <textarea id="message" name="message" required></textarea>
   </div>
+
   <button type="submit">Send</button>
 </form>
